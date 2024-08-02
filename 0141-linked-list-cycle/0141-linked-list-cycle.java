9b1e42b833462eaebe01b01fb45d1adc907a808d
Mohak Singh <72522871+mohakksingh@users.noreply.github.com>
2024-08-02 23:52:53 +0530

    Time: 0 ms (100%), Space: 44.5 MB (32.52%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -13,7 +13,7 @@ public class Solution {
     public boolean hasCycle(ListNode head) {
         ListNode fast=head;
         ListNode slow=head;
-        
+
         while(fast!=null && fast.next!=null){
             fast=fast.next.next;
             slow=slow.next;
@@ -21,7 +21,7 @@ public class Solution {
                 return true;
             }
         }
+
         return false;
-     
     }
 }
