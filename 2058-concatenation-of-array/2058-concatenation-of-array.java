eae5d9e192fd066ca78f5d0dfa5fee94ee7c9362
Mohak Singh <72522871+mohakksingh@users.noreply.github.com>
2024-08-04 03:43:24 +0530

    Time: 1 ms (96.03%), Space: 45.2 MB (41.25%) - LeetHub

--- 2058-concatenation-of-array/2058-concatenation-of-array.java
+++ 2058-concatenation-of-array/2058-concatenation-of-array.java
@@ -4,10 +4,7 @@ class Solution {
         int arr[]=new int[n*2];
 
         for(int i=0;i<n;i++){
-            arr[i]=nums[i];
-        }
-        for(int i=0;i<n;i++){
-            arr[i+n]=nums[i];
+            arr[i+n]=arr[i]=nums[i];
         }
 
         return arr;
